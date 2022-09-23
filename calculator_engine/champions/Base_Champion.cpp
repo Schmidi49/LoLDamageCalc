9f7e9af999c03a7a0e83d8ef91d5986eb7e66c98
Schmidi49 <erik.schmidi49@gmail.com>
2022-09-23 17:46:13 +0200

    created structure for basic stats of champion

---
+++ calculator_engine/champions/Base_Champion.cpp
@@ -0,0 +1,8 @@
+/******************************************************************************
+ * @file Base_Champion.cpp
+ * @author Erik Schmidthaler
+ *
+ * (C) Erik Schmidthaler (23.09.2022)
+ */
+
+#include "Base_Champion.h"
---
+++ calculator_engine/champions/Base_Champion.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * @file Base_Champion.h
+ * @author Erik Schmidthaler
+ *
+ * (C) Erik Schmidthaler (23.09.2022)
+ */
+
+#ifndef LOLDAMAGECALC_BASE_CHAMPION_H
+#define LOLDAMAGECALC_BASE_CHAMPION_H
+
+
+class Base_Champion {
+
+};
+
+
+#endif //LOLDAMAGECALC_BASE_CHAMPION_H
